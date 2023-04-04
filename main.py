91065483962e94e6c0d244aa863765a8aae75b3f
Erxleben, Fredo <f.erxleben@hzdr.de>
2023-04-04 15:27:05 +0200

    Try moving the main out of of the package
    See if it then picks up the imports properly

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ __status__ = "Production"
 import sys
 from multiprocessing import Queue, Pipe
 from PyQt5 import QtWidgets
-from app import MainWindow
+from spectra_compiler.app import MainWindow
 import pathlib
-from generator import SpectroProcess
-from workers import Emitter
+from spectra_compiler.generator import SpectroProcess
+from spectra_compiler.workers import Emitter
 
 if __name__ == "__main__":
-    icon_path = '../resources/rainbow.ico'
+    icon_path = 'resources/rainbow.ico'
     icon_path = pathlib.Path(icon_path)
     app = QtWidgets.QApplication(sys.argv)
 
